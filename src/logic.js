ca4d59401ee03ed74578fd0aac40eba45c8644a5
IsraelSanabriaMx <israel.sanabriamx@gmail.com>
2021-07-05 13:59:54 -0500

    refactor and add new step

--- src/logic.js
+++ src/logic.js
@@ -3,12 +3,54 @@ const options = {
   category: '',
   level: '',
   price: '',
+  product: '',
 };
 
 // define el minimo de caracteres en el input para buscar
 const MIN_LENGTH = 5;
 // el valor de la tecla ENTER
 const ENTER_KEY = 13;
+
+const buildProducts = (results) => {
+  let row = [];
+
+  if (results.length > 0) {
+    row = results.map((item) => (
+      `<div class="product" onclick="showPanel('product', '` + item.name + `')">
+         <img src="` + item.image + `" class="card_image_small"/>
+         <label>` + item.name + `</label>
+      </div>`
+    ));
+  }
+
+  return row.join('');
+};
+
+const searchProducts = (type, params) => {
+  showElement('results');
+  showElement('products');
+
+  const products = [
+    {
+      id: 1,
+      image: 'https://dictionary.cambridge.org/es/images/full/mobile_noun_002_23642.jpg?version=5.0.177',
+      name: 'iphone'
+    },
+    {
+      id: 2,
+      image: 'https://dictionary.cambridge.org/es/images/full/mobile_noun_002_23642.jpg?version=5.0.177',
+      name: 'samsung'
+    },
+    {
+      id: 3,
+      image: 'https://dictionary.cambridge.org/es/images/full/mobile_noun_002_23642.jpg?version=5.0.177',
+      name: 'huawei'
+    },
+  ];
+
+  const build = buildProducts(products);
+  document.getElementById('products').innerHTML = build;
+};
 
 const buildRow = (results) => {
   let row = [];
@@ -27,11 +69,12 @@ const buildRow = (results) => {
   }
 
   return row.join('');
-}
+};
 
 // llamado al api para buscar
 const search = (type, params) => {
-  showElement('results');
+  console.log(type, params);
+  hideElement('products');
 
   const results = [
     {
@@ -65,19 +108,19 @@ const search = (type, params) => {
 
   const build = buildRow(results);
   document.getElementById('comparator').innerHTML = build;
-}
+};
 
 // agrega el estilo para mostrar
 const showElement = (id) => {
   const e = document.getElementById(id);
   e.style.display = 'block';
-}
+};
 
 // agrega el estilo para ocultar
 const hideElement = (id) => {
   const e = document.getElementById(id);
   e.style.display = 'none';
-}
+};
 
 // función para setear los parametros y mostrar el siguiente panel
 const showPanel = (type, opt) => {
@@ -110,40 +153,47 @@ const showPanel = (type, opt) => {
   if (type === 'price') {
     // llama al api para consultar con los parametros
     hideElement('prices');
+    searchProducts('options', options);
+  }
+
+  if (type === 'product') {
+    hideElement('prices');
     search('options', options);
   }
-}
+};
 
 // regresa a su valor inicial los parametros y comienza de nuevo
 const resetSearch = () => {
   options.category = '';
   options.level = '';
   options.price = '';
+  options.product = '';
 
   showElement('categories');
   hideElement('results');
   document.getElementById('comparator').innerHTML = '';
-}
+};
 
 const getInputValue = () => {
   const param = document.getElementById('search_text').value;
 
   return param.trim();
-}
+};
 
 // obtine el valor del input y si cumple la longitud minima llama la busqueda
 const callSearchFn = (type) => {
   const param = getInputValue();
   if (param.length >= MIN_LENGTH) {
+    showElement('results');
     search(type, param);
   } else {
     alert('Agrega más caracteres y vuelve a intentarlo');
   }
-}
+};
 
 // cada que teclea en el input valida si da enter y llama a la busqueda si cumple con la longitud minima
 const callSearch = (e) => {
-  if (e.keyCode === 13) {
+  if (e.keyCode === ENTER_KEY) {
     callSearchFn('text')
   }
-}
+};
